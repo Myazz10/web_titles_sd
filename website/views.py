e81959272639234cf0a2f33560b757343cdc102d
Moi Myazz <moimyazz@gmail.com>
2021-04-28 02:07:41 -0500

    spider function update test

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib import messages
 from django.shortcuts import render
 import time
@@ -11,9 +13,18 @@ from website.models import Website
 
 def spider(request, url):
     options = Options()
-    options.add_argument('--headless')
+    '''options.add_argument('--headless')
     options.add_argument('--disable-gpu')  # Last I checked this was necessary.
-    driver = webdriver.Chrome(executable_path=binary_path, chrome_options=options)
+    driver = webdriver.Chrome(executable_path=binary_path, chrome_options=options)'''
+
+    options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--remote-debugging-port=9222')
+
+    driver = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=options)
 
     try:
         driver.get(url)
